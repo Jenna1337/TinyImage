53b57656908871f584f2f71d8673aefb8b907626
Jonah Sloan <jonah.m.sloan@gmail.com>
2016-02-02 12:31:33 -0600

    Started on ScaleablePixelBox

--- src/tinyImage/designer/ScaleablePixelBox.java
+++ src/tinyImage/designer/ScaleablePixelBox.java
@@ -11,12 +11,20 @@ public class ScaleablePixelBox extends JPanel implements MouseInputListener
 	//for use in palette and main image design screen
 	//TODO add a clickable colorable box that can execute a command
 	//might have to be abstract
+	private boolean mouseisover=false;
+	private boolean mousedown=false;
+	protected ThreeBitColor color;
 	
+	ActionThread thread;
+	
+	public ScaleablePixelBox(ActionThread thread, ThreeBitColor colorinitss)
+	{
+		//TODO
+	}
 	public void mouseClicked(MouseEvent e)
 	{
 		// TODO Auto-generated method stub
 	}
-
 	public void mouseEntered(MouseEvent e)
 	{
 		// TODO Auto-generated method stub
@@ -25,7 +33,6 @@ public class ScaleablePixelBox extends JPanel implements MouseInputListener
 	{
 		// TODO Auto-generated method stub
 	}
-	
 	public void mousePressed(MouseEvent e)
 	{
 		// TODO Auto-generated method stub
@@ -34,13 +41,37 @@ public class ScaleablePixelBox extends JPanel implements MouseInputListener
 	{
 		// TODO Auto-generated method stub
 	}
-	
-	public void mouseDragged(MouseEvent arg0)
-	{
-		// TODO Auto-generated method stub
+	public void mouseDragged(MouseEvent arg0){}
+	public void mouseMoved(MouseEvent arg0){}
+	public boolean isMouseOver() {
+		return mouseisover;
 	}
-	public void mouseMoved(MouseEvent arg0)
+	public boolean isMouseDown() {
+		return mousedown;
+	}
+}
+enum ThreeBitColor
+{
+	//       red  green  blue
+	BLACK  (false,false,false),//000 
+	RED    (true, false,false),//100 
+	GREEN  (false, true,false),//010 
+	YELLOW ( true, true,false),//110 
+	BLUE   (false,false, true),//001 
+	MAGENTA( true,false, true),//101 
+	CYAN   (false, true, true),//011 
+	WHITE  ( true, true, true);//111 
+	boolean r,g,b;
+	ThreeBitColor(boolean red, boolean green, boolean blue)
 	{
-		// TODO Auto-generated method stub
+		this.r=red; this.g=green; this.b=blue;
+	}
+	public boolean[] getRGB()
+	{
+		return new boolean[]{this.r, this.g, this.b};
+	}
+	public java.awt.Color getColor()
+	{
+		return new java.awt.Color(r?1.0f:0.0f, g?1.0f:0.0f, b?1.0f:0.0f);
 	}
 }
